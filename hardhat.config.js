0548b343f1dddb897dc6b24911d2d12dedd3e57c
jochenruland <jochenhgruland@gmail.com>
2023-02-13 08:07:43 +0100

    ADD mainnet fork and mocha configuration to hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const fs = require('fs');
+const infuraKey = fs.readFileSync(".secret").toString().trim();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://mainnet.infura.io/v3/${infuraKey}`,
+        //url: `https://polygon-mainnet.infura.io/v3/${infuraKey}`,
+      }
+    },
+  },  
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    timeout: 100000
+  },
+  // Configure your compilers
+  solidity: {
+    version: "0.8.17"
+  }
 };
